eb0510199afb42121d281d6efc7e5fa5df49f45c
jlifs <jlifs74@gmail.com>
2025-06-26 16:36:11 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Jennifer Lifsey and is{" "}
-        <a
-          href="https://github.com/jlifs/react-weather-forecast-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Jennifer Lifsey and is{" "}
+          <a
+            href="https://github.com/jlifs/react-weather-forecast-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
