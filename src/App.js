2be7d92011cafac09ea633ff337239a7ba3af9c4
jlifs <jlifs74@gmail.com>
2025-06-26 15:46:18 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
+
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Jennifer Lifsey and is{" "}
+        <a
+          href="https://github.com/jlifs/react-weather-forecast-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
